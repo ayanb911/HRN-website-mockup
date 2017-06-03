2302ad90d5482453a22edc1e34822aed6a3d0378
ayan <ayan7414@gmail.com>
2017-06-03 08:58:41 +0530

    Work icon animation done, repeats after completing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
 $(document).ready(function () {
-    setTimeout(autoIconScale, 14);
+    autoIconScale(); //work icon animation function call
 })
 
-function autoIconScale(){
-    console.log("icons");
+function autoIconScale(){ //future of work icon animation function
     var workIcon = $(".work-icon");
     workIcon.each(function (i) {
         var workIconEach = $(this),
-            notWorkIcon = $(".work-icon").not(workIconEach);
+            notThis = $(".work-icon").not(workIconEach);
+
         setTimeout(function () {
+            //add classes to 'this'
             workIconEach.find("div.work-icon--title").addClass("show");
             workIconEach.find("div.work-icon--img").addClass("hovered");
-            notWorkIcon.find("div.work-icon--title").removeClass("show");
-            notWorkIcon.find("div.work-icon--img").removeClass("hovered");
+
+            //remove classes from other elements
+            notThis.find("div.work-icon--title").removeClass("show");
+            notThis.find("div.work-icon--img").removeClass("hovered");
+
+            /*
+            Debugging Purpose
+            console.log("each i = ", i);
+            */
         }, 1000*i);
 
     });
+    setTimeout(function () { //restart the function after completion of first setTimeout
+        autoIconScale();
+    }, 1000 * workIcon.length-1);
 }
