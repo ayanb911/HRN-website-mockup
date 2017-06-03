f2f39e01f71c93b44f7263b91bd30dae08ded686
ayan <ayan7414@gmail.com>
2017-06-03 21:35:35 +0530

    mobile navbar completely done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     autoIconScale(); //Icon animation - Future of Work
-    hamToCross();
-    menuMore();
+    menuAnimate(); //hamburger to cross icon transition
+    menuMore();//nav menu for mobile more click
 })
 
 //Icon animation - Future of Work
@@ -28,10 +28,40 @@ function autoIconScale(){
 }
 
 //Hamburger icon to cross animation
-function hamToCross(){
-    $("#hamburgerBtn").click(function () {
+function menuAnimate(){
+
+    //variables
+    var $ham = $("#hamburgerBtn"),
+        $menuItem = $("li.menu-item"),
+        click = false;
+
+    $ham.click(function () {
+        $ham.toggleClass("isCross")
         //toggle class to make it cross and for menu to slide in
-        $(this).toggleClass("isCross").parent().find("#mobileMenu").toggleClass("isOpen");
+        click = !click;
+        if(click){
+            $ham.parent().find("#mobileMenu").addClass("isOpen");
+            $menuItem.each(function (i) {
+                var menuItemThis = $(this),
+                    notThis = $menuItem.not(menuItemThis);
+                console.log(this);
+                setTimeout(function () {
+                    menuItemThis.addClass("isEntering");
+                }, 30*i);
+            })
+        }
+        else{
+            $menuItem.each(function (i) {
+                var menuItemThis = $(this),
+                    notThis = $menuItem.not(menuItemThis);
+                setTimeout(function () {
+                    menuItemThis.removeClass("isEntering");
+                }, 30*i);
+            })
+            setTimeout(function () {
+                $ham.parent().find("#mobileMenu").removeClass("isOpen");
+            }, 30 * ($menuItem.length-1));
+        }
     })
 }
 
